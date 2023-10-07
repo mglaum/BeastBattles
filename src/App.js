d10c3993d1858a27833c0c137ccf8c9cf10f29be
Maddie Glaum <maddieglaum@Maddies-MacBook-Air-2.local>
2023-10-07 19:46:03 -0400

    Okay this should work or i cry

--- src/App.js
+++ src/App.js
@@ -1,40 +1,10 @@
-import React, { useState } from 'react';
-import { FighterData } from './FighterData';
+import React from 'react';
 import './App.css';
+import Landing from './pages/Landing/Landing';
 
 function App() {
-  const [randomIndices, setRandomIndices] = useState([]);
-  
-  // Function to generate two unique random indices
-  const getRandomIndices = () => {
-    const indices = [];
-    while (indices.length < 2) {
-      const newIndex = Math.floor(Math.random() * FighterData.length);
-      if (!indices.includes(newIndex)) {
-        indices.push(newIndex);
-      }
-    }
-    setRandomIndices(indices);
-  };
-
   return (
-    <div className="container">
-      <button onClick={getRandomIndices}>Pick Two Random Fighters</button>
-      {randomIndices.length === 2 && (
-        <div className="fighter-container">
-          {randomIndices.map((index) => (
-            <div key={index}>
-              <img
-                src={FighterData[index].image}
-                alt={FighterData[index].name}
-                className="fighter-image"
-              />
-              <p className="fighter-name">{FighterData[index].name}</p>
-            </div>
-          ))}
-        </div>
-      )}
-    </div>
+    <Landing/>
   );
 }
 
---
+++ src/pages/Landing/Landing.js
@@ -0,0 +1,15 @@
+import "./Landing.css"
+
+const Landing = () => {
+  return (
+    <div className="background">
+     <div className="fire"> 
+        BEAST BATTLES 
+      </div>
+     </div>
+     
+  
+  );
+};
+
+export default Landing;
