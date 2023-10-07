c22fa0394e7f49120bccc160503226f68c8784d8
Maddie Glaum <maddieglaum@Maddies-MacBook-Air-2.local>
2023-10-07 16:56:44 -0400

    lrfdejdi

---
+++ src/assets/fighters/FighterData.js
@@ -0,0 +1,10 @@
+export const FighterData = [
+    {
+        image: require("./FighterImages/buffcharlie.jpeg"), 
+        name: "Buff Charlie"
+    }, 
+    {
+        image: require("./FighterImages/brothersthree.jpeg"),
+        name: "Brothers Three", 
+    }
+]
