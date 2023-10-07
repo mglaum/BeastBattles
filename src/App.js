d500e4afcf550510ea55aee40782fb4b283c2b9d
duniahamed <93282302+duniahamed@users.noreply.github.com>
2023-10-07 17:24:42 -0400

    work

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import React, { useState } from 'react';
 import { FighterData } from './FighterData';
-import './App.css'; // Import your App.css stylesheet
+import './App.css';
 
 function App() {
-  const [randomIndex, setRandomIndex] = useState(null);
-
-  const getRandomIndex = () => {
-    const newIndex = Math.floor(Math.random() * FighterData.length);
-    setRandomIndex(newIndex);
+  const [randomIndices, setRandomIndices] = useState([]);
+  
+  // Function to generate two unique random indices
+  const getRandomIndices = () => {
+    const indices = [];
+    while (indices.length < 2) {
+      const newIndex = Math.floor(Math.random() * FighterData.length);
+      if (!indices.includes(newIndex)) {
+        indices.push(newIndex);
+      }
+    }
+    setRandomIndices(indices);
   };
 
   return (
     <div className="container">
-      <button onClick={getRandomIndex}>Get Random Fighter</button>
-      {randomIndex !== null && (
-        <div>
-          <img
-            src={FighterData[randomIndex].image.default}
-            alt={FighterData[randomIndex].name}
-          />
-          <p>{FighterData[randomIndex].name}</p>
+      <button onClick={getRandomIndices}>Pick Two Random Fighters</button>
+      {randomIndices.length === 2 && (
+        <div className="fighter-container">
+          {randomIndices.map((index) => (
+            <div key={index}>
+              <img
+                src={FighterData[index].image.default}
+                alt={FighterData[index].name}
+                className="fighter-image"
+              />
+              <p className="fighter-name">{FighterData[index].name}</p>
+            </div>
+          ))}
         </div>
       )}
     </div>
