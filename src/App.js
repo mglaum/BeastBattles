31c9c80a4bf2f1f586c4dfce73ecf88705cf1d1f
duniahamed <93282302+duniahamed@users.noreply.github.com>
2023-10-07 17:05:49 -0400

    s

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import GetName from './GetName';
-import './App.css';
+import React, { useState } from 'react';
+import { FighterData } from './FighterData';
+import './App.css'; // Import your App.css stylesheet
 
 function App() {
+  const [randomIndex, setRandomIndex] = useState(null);
+
+  const getRandomIndex = () => {
+    const newIndex = Math.floor(Math.random() * FighterData.length);
+    setRandomIndex(newIndex);
+  };
+
   return (
-    <div>
-      <GetName/>
+    <div className="container">
+      <button onClick={getRandomIndex}>Get Random Fighter</button>
+      {randomIndex !== null && (
+        <div>
+          <img
+            src={FighterData[randomIndex].image.default}
+            alt={FighterData[randomIndex].name}
+          />
+          <p>{FighterData[randomIndex].name}</p>
+        </div>
+      )}
     </div>
   );
 }
