f341c0d1d1cad145de46aa2938fe39c33a0f605b
duniahamed <93282302+duniahamed@users.noreply.github.com>
2023-10-08 09:25:32 -0400

    okurt

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React from 'react';
 import './App.css';
 import Landing from './pages/Landing/Landing';
 import FighterGenerator from './FighterGenerator';
-import { Route, Routes } from 'react-router-dom'; 
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; 
 import Roster from './pages/Roster/Roster';
+import Fighter0 from "./pages/Fighters/Fighter0";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Route path='/' element={ <Landing/> } />
       <Route path='/test' element={ <FighterGenerator/> } />
       <Route path='/roster' element={<Roster/>} />
+      <Route path="/:id" element={<Fighter0 />} /> {/* Updated route */}
     </Routes>
     // <FighterGenerator/>
   );
--- src/pages/Fighters/Fighter0.js
+++ src/pages/Fighters/Fighter0.js
@@ -1,15 +1,27 @@
-import "./Fighters.css"
-import { useNavigate } from "react-router-dom";
+import React from "react";
 import { FighterData } from "../../FighterData";
+import { useParams } from "react-router-dom";
+import "./Fighters.css";
 
 const Fighter0 = () => {
-  const navigate = useNavigate();
+  const { id } = useParams();
+  const fighter = FighterData[id];
 
-  const handleClick = () => {
-    navigate("/");
-  };
+  if (!fighter) {
+    return <div>Fighter not found</div>;
+  }
+
   return (
-<div></div>
+    <div className="fighter-page">
+      <div className="fighter-details">
+        <div className="fighter-image">
+          <img src={fighter.image} alt={fighter.name} className="zoom-in" />
+        </div>
+        <div className="fighter-name">
+          <p>{fighter.name}</p>
+        </div>
+      </div>
+    </div>
   );
 };
 
